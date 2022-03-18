c4c9010f807e5e490ad8ae06e48598660462ad24
immemx <eayresm4@gmail.com>
2022-03-18 00:49:42 -0400

    terminate connection works! running into issues with DB

--- db/connection.js
+++ db/connection.js
@@ -3,17 +3,25 @@ const mysql = require('mysql2');
 // Connecting .env file to I can hide my credentials from being posted to github
 require('dotenv').config();
 
+// The Below style of creating a connection does not work with .end() method?? strange
+// const dbConfig = {
+//     host: 'localhost',
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PW,
+//     database: process.env.DB_NAME
+// };
+
+// // Connect to databse
+// const db = mysql.createConnection(dbConfig,
+//     console.log(`Now connected to ${process.env.DB_NAME}`)
+// ).promise // <== Enable promise based syntax
+
 // database config, Also using the dotenv npm to hide password and user in .env
-const dbConfig = {
+const connection = mysql.createConnection({
     host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PW,
     database: process.env.DB_NAME
-};
+});
 
-// Connect to databse
-const db = mysql.createConnection(dbConfig,
-    console.log(`Now connected to ${process.env.DB_NAME}`)
-).promise // <== Enable promise based syntax
-
-module.exports = db;
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // Require statments
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-const db = require('./db/connection.js');
-
-// Link to SQL switch in utils folder
-const sql_switch = require('./utils/sql_switch')
+const connection = require('./db/connection.js');
 
 // Display the starting program message
 const startScreen = () => {
@@ -29,12 +26,59 @@ const prompt = () => {
 
         // Check to see if quit was selected
         if(answers.options === 'quit') {
+            connection.end();
             return;
         };
 
         // Pass Choice into switch function to respond to answer
         sql_switch(answers);
     })
+}
+
+// Switch for main prompt
+const sql_switch = (answer) => {
+
+    // Switch processing the selection from inquirer and redirecting.
+    switch (answer.options) {
+    // Case for seeing ALL DEPT.
+        case 'view all departments':
+            const sqlAllDept = `SELECT * FROM department`
+            connection.query(sqlAllDept, (err, result) => {
+                if(err) {
+                    console.log(`There has been an error: ${err}`)
+                    return;
+                }
+                console.table(result)
+                prompt();
+            })
+    // Case for seeing ALL ROLES
+        case 'view all roles':
+            const sqlAllRoles = `SELECT * FROM role`
+            db.query(sqlAllRoles, (err, result) => {
+                if(err) {
+                    console.log(`There has been an error: ${err}`)
+                    return;
+                }
+                console.table(result)
+                return;
+            })
+            return;
+    // Case for seeing ALL EMPLE.
+        case 'view all employees':
+            return;
+    // Case for ADDing a DEPT.
+        case 'add a department':
+            return;
+
+        case 'add a role':
+            return;
+
+        case 'add an employee':
+            return;
+
+        case 'update an employee role':
+            return;
+    };
 }
 
 startScreen();
